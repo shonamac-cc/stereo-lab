ddf00931c2de92707f3c4c7cd72dccdc413e9605
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-10-15 12:46:47 +0100

    added component abstract class

--- src/main/java/Component.java
+++ src/main/java/Component.java
@@ -1,2 +1,12 @@
 public abstract class Component {
+    protected String make;
+    protected String model;
+
+    public String make(){
+        return make;
+    }
+
+    public String getModel(){
+        return model;
+    }
 }
